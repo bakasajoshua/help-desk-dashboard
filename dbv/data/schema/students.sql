ab35170d8b7b69c21790c19d14d1aaeaaa22964f
mugambi1tech <symonkenancy@gmail.com>
2018-08-22 16:18:34 +0300

    created students table

---
+++ dbv/data/schema/students.sql
@@ -0,0 +1,7 @@
+CREATE TABLE `students` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` text NOT NULL,
+  `gender` text NOT NULL,
+  `age` int(11) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1
