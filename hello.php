1146445c7e3db32128b6a325be2564c3ec0cc428
mugambi1tech <symonkenancy@gmail.com>
2018-08-22 15:07:34 +0300

    add html table

--- hello.php
+++ hello.php
@@ -4,6 +4,28 @@
 	<title>Hello World</title>
 </head>
 <body>
-	<?= @"Hello World"; ?>
+<table>
+	<tr>
+		<td>name</td>
+		<td>john</td>
+		<td>peter</td>
+		<td>jane</td>
+		<td>sammy</td>
+		<tr>
+			<td>age</td>
+			<td>13</td>
+			<td>9</td>
+			<td>23</td>
+			<td>18</td>
+		</tr>
+		<tr>
+			<td>gender</td>
+			<td>male</td>
+			<td>male</td>
+			<td>female</td>
+			<td>male</td>
+		</tr>
+		
+
 </body>
 </html>
